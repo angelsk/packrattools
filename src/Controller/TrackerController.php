019041251f9ed5aedea8ada336cce5ca9d8f4702
Jo Carter <github@jocarter.co.uk>
2019-01-26 23:28:16 +0000

    Adding original home page (copy pasta) + styles etc

---
+++ src/Controller/TrackerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrackerController extends AbstractController
+{
+    /**
+     * @Route("/tracker", name="tracker")
+     */
+    public function index()
+    {
+        return $this->render('tracker/index.html.twig', [
+            'controller_name' => 'TrackerController',
+        ]);
+    }
+}
