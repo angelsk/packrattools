7ff81b23a73076105e35239cc2ff25b8967457de
Jo Carter <github@jocarter.co.uk>
2020-08-27 18:49:52 +0100

    Add React and Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,20 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    .enableReactPreset()
+
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
+
+    .addStyleEntry('tailwind', './assets/css/tailwind.css')
+    // enable post css loader
+    .enablePostCssLoader((options) => {
+        options.config = {
+            // directory where the postcss.config.js file is stored
+            path: './postcss.config.js'
+        };
+    })
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
